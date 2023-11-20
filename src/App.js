0cc49e402cca1f4fb78d53fcc0b4b1985b1e3d38
Carson Barry <carsonbarry@MacBook-Pro.local>
2023-11-20 15:39:39 -0600

    created navbar component, added routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import key from './APIKey';
+import Navbar from "./Navbar";
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>{key}</div>
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="hello" element={<Navbar/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    
+    return (
+        <div className="Navbar">
+            <h1 className="Navbar-title"><a href="/" className="Navbar-link">PC.cool</a></h1>
+            <hr className="Navbar-hr"></hr>
+        </div>
+    )
+}
+
+export default Navbar;
